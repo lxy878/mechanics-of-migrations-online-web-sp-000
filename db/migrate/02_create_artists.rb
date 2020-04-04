d8e0500ee0bb9c64e18a2826262686ee8eb32e5d
Xiaoyi Liu <lxy878@gmail.com>
2020-04-04 15:23:05 +0000

    Automatically backed up by Learn

--- db/migrate/02_create_artists.rb
+++ db/migrate/02_create_artists.rb
@@ -6,11 +6,6 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   end
 
   def change
-    create_table :artists do |t|
-      t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
-    end
+    add_column :artists, :favorite_food, :string
   end
 end
